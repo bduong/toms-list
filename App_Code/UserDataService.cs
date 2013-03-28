882cdde41d6bb14ee4757ac3947a524a2c3bef60
Ben Duong <benjaminf.duong@gmail.com>
2013-03-28 14:53:32 -0400

    Added UserDataService as test

---
+++ App_Code/UserDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserDataService
+/// </summary>
+public class UserDataService
+{
+	public UserDataService()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
