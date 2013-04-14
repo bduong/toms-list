4531a704dfb9caca98ffd4ffb0921e1c8a636aac
Ben Duong <benjaminf.duong@gmail.com>
2013-04-14 13:23:35 -0400

    Added tags data service

---
+++ App_Code/Entities/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Tag
+/// </summary>
+public class Tag
+{
+    public int id { get; set; }
+    public string name { get; set; }
+
+	public Tag(int id, string name)
+	{
+        this.id = id;
+        this.name = name;
+	}
+}
