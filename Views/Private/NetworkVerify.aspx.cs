3144f62d8e5f225b777891f83fd5d471826ac9af
Ben Duong <benjaminf.duong@gmail.com>
2013-04-22 11:08:33 -0400

    Forgot to add new files for network verification

---
+++ Views/Private/NetworkVerify.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class Views_Private_NetworkVerify : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Guid test;
+        if (string.IsNullOrEmpty(Request.QueryString["ID"]) || !Guid.TryParse(Request.QueryString["ID"], out test))
+        {
+            Info.Text = "Invalid ID";
+            return;
+        }
+       
+       
+        Guid userId = new Guid(Request.QueryString["ID"]);
+        if (Membership.GetUser(userId) == null)
+        {
+            Info.Text = "User does not exist";
+            return;
+        }
+
+        MembershipUser loggedInUser = Membership.GetUser();
+        Guid loggedInGuid = (Guid)loggedInUser.ProviderUserKey;
+        if (!loggedInGuid.ToString().Equals(userId.ToString()))
+        {
+            Info.Text = "Incorrect User ID";
+            return;
+        }
+
+        int networkId = 0;
+        try
+        {
+            networkId = Convert.ToInt32(Request.QueryString["N"]);
+        }
+        catch
+        {
+            Info.Text = "Invalid Network";
+        }
+
+        if (networkId >= 0)
+        {
+            Network network = NetworkDataService.getNetwork(networkId);
+            User user = UserDataService.getUser(userId);
+
+            if (network == null)
+            {
+                Info.Text = "Network does not exist";
+            }
+            else if (NetworkDataService.doesUserBelongToNetwork(user, network)) {
+                Info.Text = user.name + " is already part of " + network.name;
+            }
+            else if (UserDataService.addUserToNetwork(user, network))
+            {
+                Info.Text = user.name + " has been added to " + network.name;
+            }
+            else
+            {
+                Info.Text = "We cannot add you to the network at this time";
+            }
+            
+        }
+    }
+}
---
+++ Views/Private/Networks.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+using System.IO;
+using System.Web.Security;
+
+public partial class Views_Private_Networks : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Do_Search_Click(object sender, EventArgs e)
+    {
+        ErrorMessage.Text = "";
+        string searchPattern = Network_Search.Text;
+        Network_Search.Text = "";
+        List<Network> networks = NetworkDataService.searchForNetworksByName(searchPattern);        
+        Results.Items.Clear();
+        foreach (Network n in networks)
+        {
+            Results.Items.Add(new ListItem(n.name, n.id.ToString()));
+        }
+        if (networks.Count > 0)
+        {
+            Join.Visible = true;
+        }
+        else
+        {
+            Join.Visible = false;
+        }
+    }
+
+    protected void Join_Click(object sender, EventArgs e)
+    {
+        ErrorMessage.Text = "";
+        if(Results.SelectedIndex >= 0) {
+            int networkId = Convert.ToInt32(Results.SelectedValue);
+            Network network = NetworkDataService.getNetwork(networkId);
+            Domain.Text = "Email Domain Required: " + network.pattern;
+            Email.Text = "";
+            toggleVisibilityOfJoinControls(true);            
+        }
+
+    }
+
+    private void toggleVisibilityOfJoinControls(Boolean visible)
+    {        
+        Results.Enabled = !visible;        
+        Domain.Visible = visible;
+        Info.Visible = visible;
+        Email.Visible = visible;
+        Send.Visible = visible;
+        Cancel.Visible = visible;
+    }
+
+    protected void Send_Click(object sender, EventArgs e)
+    {
+        int networkId = Convert.ToInt32(Results.SelectedValue);
+        Network network = NetworkDataService.getNetwork(networkId);
+
+        string email = Email.Text;
+        try
+        {
+            new MailAddress(email);
+        }
+        catch
+        {
+            ErrorMessage.Text = "Not a valid email address";
+            return;
+        }
+
+        if (email.EndsWith("@" + network.pattern, false, null))
+        {
+            ErrorMessage.Text = "Verification Email Sent!";
+            sendMail(email, network);
+        }
+        else
+        {
+            ErrorMessage.Text = "Email does belong to the correct domain";
+        }
+    }
+
+    protected void Cancel_Click(object sender, EventArgs e)
+    {
+        ErrorMessage.Text = "";
+        toggleVisibilityOfJoinControls(false);
+    }
+
+    private void sendMail(string email, Network network)
+    {
+        MailMessage mailMessage = new MailMessage();
+        mailMessage.From = new MailAddress("toms.list.verify@gmail.com");
+        mailMessage.Subject = "Network Verification";
+        mailMessage.To.Add(new MailAddress(email));
+        string body = "";
+        using (StreamReader reader = new StreamReader(Server.MapPath("~/Templates/AddNetworkTemplate.html")))
+        {
+            body = reader.ReadToEnd();
+        }
+
+        MembershipUser user = Membership.GetUser();
+        Guid userId = (Guid) user.ProviderUserKey;
+        body = body.Replace("<%UserName%>", user.UserName);
+        body = body.Replace("<%NetworkName%>", network.name);
+
+        string baseUrl =  Request.Url.GetLeftPart(UriPartial.Authority) + (Request.ApplicationPath.Equals("/") ? "" : Request.ApplicationPath);
+        string verifyUrl = "/Views/Private/NetworkVerify.aspx?ID="+ userId.ToString() + "&N="+network.id.ToString();
+
+        body = body.Replace("<%VerifyUrl%>", baseUrl+verifyUrl);
+        mailMessage.Body = body;
+        mailMessage.IsBodyHtml = true;
+
+        new SmtpClient().Send(mailMessage);
+    }
+}
