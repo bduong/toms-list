95778c45d821463b16b6db15d5780fd829d93dfc
henry <huanghe@bu.edu>
2013-04-23 17:22:54 -0400

    garagesale files

---
+++ App_Code/DataServices/GarageDataService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for GarageDataServices
+/// </summary>
+public class GarageDataService
+{
+	public GarageDataService()
+	{
+	
+	}
+
+    public static Garage getGarageSale(string id)
+    {
+        SqlConnection conn = DBConnector.getSqlConnection();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM GarageSale where GarageID = @GarageId", conn);
+        cmd.Parameters.AddWithValue("@GarageId", id);
+
+        SqlDataReader reader = cmd.ExecuteReader();
+        reader.Read();
+        Garage returnGarage = extractGS(reader);
+        conn.Close();
+
+        return returnGarage;
+    }
+
+    public static Garage addGarageSale(Garage gs)
+    {
+        SqlConnection conn = DBConnector.getSqlConnection();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("INSERT INTO GarageSale (UserID, DateBegin, DateEnd, Address, Description) VALUES (@UserId, @DateBegin, @DateEnd, @Address, @Description); SELECT SCOPE_IDENTITY()", conn);
+        cmd.Parameters.AddWithValue("@UserId", gs.userID);
+        cmd.Parameters.AddWithValue("@DateBegin", gs.DateBegin);
+        cmd.Parameters.AddWithValue("@DateEnd", gs.DateEnd);
+        cmd.Parameters.AddWithValue("@Address", gs.Address);
+        cmd.Parameters.AddWithValue("@Description", gs.Description);
+        int uid = Convert.ToInt32(cmd.ExecuteScalar());
+        conn.Close();
+        gs.GarageID = uid;
+
+        return gs;
+    }
+
+    public static Boolean deleteGarageSale(String id)
+    {
+        return false; //placeholder
+    }
+
+    public static Boolean updateGarageSale(String id)
+    {
+        return false; //placeholder
+    }
+
+    public static class ColumnNames
+    {
+        public static string GarageId = "GarageID";
+        public static string UserId = "UserID";
+        public static string DateBegin = "DateBegin";
+        public static string DateEnd = "DateEnd";
+        public static string Address = "Address";
+        public static string Description = "Description";
+    }
+    private static Garage extractGS(SqlDataReader reader)
+    {
+
+        int uid = (int)reader[ColumnNames.GarageId];
+        Guid userId = (Guid)reader[ColumnNames.UserId];
+        DateTime dateb = (DateTime)reader[ColumnNames.DateBegin];
+        DateTime datee = (DateTime)reader[ColumnNames.DateEnd];
+        string address = (string)reader[ColumnNames.Address];
+        string description = (string)reader[ColumnNames.Description];
+        return new Garage(userId, dateb, datee, address, description);
+    }
+}
---
+++ Views/Private/GarageSale.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class Views_Private_GarageSale : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (textbox_begin.Text != ""
+            && textbox_date.Text != ""
+            && textbox_end.Text != ""
+            && textbox_location.Text != "")
+        {
+            try
+            {
+
+                string[] dateday = textbox_date.Text.Split('/');
+                string[] datetime_begin = textbox_begin.Text.Split(':');
+                string[] datetime_end = textbox_end.Text.Split(':');
+
+                DateTime begintime = new DateTime(Convert.ToInt32(dateday[0]), Convert.ToInt32(dateday[1]), Convert.ToInt32(dateday[2]), Convert.ToInt32(datetime_begin[0]), Convert.ToInt32(datetime_begin[1]), 0);
+                DateTime endtime = new DateTime(Convert.ToInt32(dateday[0]), Convert.ToInt32(dateday[1]), Convert.ToInt32(dateday[2]), Convert.ToInt32(datetime_end[0]), Convert.ToInt32(datetime_end[1]), 0);
+
+                MembershipUser user = Membership.GetUser();
+                Guid userId = (Guid)user.ProviderUserKey;
+
+            }
+            catch (Exception ex)
+            {
+                creategarage_output.Text = ex.Message;
+            }
+
+
+        }
+    }
+}
