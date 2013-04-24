4992bdb3bda0e2d5e51ba0b4c0cb950900cdffdd
Ben Duong <benjaminf.duong@gmail.com>
2013-04-24 02:22:13 -0400

    Added 500 error message

--- Views/Error.aspx.cs
+++ Views/Error.aspx.cs
@@ -13,11 +13,20 @@ public partial class Views_Error : System.Web.UI.Page
 
         switch (error_code)
         {
+            case "400":
+            case "401":
+            case "404":
             case "403":
                 error_image.ImageUrl = "~/public/img/errors/forbidden.jpg";
                 error_caption.Text = " Where do you think you're going? ";                
                 error_description.Text = "403: You are forbidden from accessing this page";
                 error_description.Text += "<br />Try logging in";
+                break;
+            case "500":
+                error_image.ImageUrl = "~/public/img/errors/server_error_500.jpg";
+                error_caption.Text = " Well this is embarrassing.....  We dun goof";
+                error_description.Text = "500: Internal Server Error";
+                error_description.Text += "<br /> ... our bad";
                 break;
             default:
                 break;
